f50e3586ee2a6de82de211bf1d87c4960bbda220
AndriyChernienko <chernienkoandriy@gmail.com>
2017-03-03 13:51:20 +0200

    Added TaskService, TaskController to achievement

--- src/main/java/com/royalrangers/service/AchievementService.java
+++ src/main/java/com/royalrangers/service/AchievementService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface AchievementService {
     public Achievement addAchievement(Achievement achievement);
+
     public Achievement getAchievementById(Long id);
+
     public void delete(Long id);
+
     public Achievement editAchievement(Achievement achievement);
+
     public List<Achievement> getAll();
 }
