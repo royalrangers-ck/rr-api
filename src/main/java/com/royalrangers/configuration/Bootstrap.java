4780fe2907aeb53b422d83b257b4698db4d62322
Lexa66 <alex341@ukr.net>
2017-03-05 12:03:11 +0200

    Populate initial data for country, city, group, platoon and section

--- src/main/java/com/royalrangers/configuration/Bootstrap.java
+++ src/main/java/com/royalrangers/configuration/Bootstrap.java
@@ -1,8 +1,6 @@
 package com.royalrangers.configuration;
 
-import com.royalrangers.model.Authority;
-import com.royalrangers.model.AuthorityName;
-import com.royalrangers.model.User;
+import com.royalrangers.model.*;
 import com.royalrangers.security.repository.AuthorityRepository;
 import com.royalrangers.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,23 +42,41 @@ public class Bootstrap {
             user.setEnabled(true);
             user.setLastPasswordResetDate(new Date());
             Authority authority = new Authority();
-            authority.setUsers(new HashSet<User>() {{ add(user); }});
+            authority.setUsers(new HashSet<User>() {{
+                add(user);
+            }});
             switch (element) {
                 case 1:
                     authority.setName(AuthorityName.ROLE_USER);
+                    user.setCountry(new Country("Ukraine"));
+                    user.setCity(new City(user.getCountry(), "Cherkasy"));
+                    user.setGroup(new Group(user.getCity(), "group1"));
+                    user.setPlatoon(new Platoon(user.getGroup(), "platoon1"));
+                    user.setSection(new Section(user.getPlatoon(), "section1"));
                     break;
                 case 2:
                     authority.setName(AuthorityName.ROLE_ADMIN);
+                    user.setCountry(new Country("USA"));
+                    user.setCity(new City(user.getCountry(), "Miami"));
+                    user.setGroup(new Group(user.getCity(), "group2"));
+                    user.setPlatoon(new Platoon(user.getGroup(), "platoon2"));
+                    user.setSection(new Section(user.getPlatoon(), "section2"));
                     break;
                 case 3:
                     authority.setName(AuthorityName.ROLE_SUPER_ADMIN);
+                    user.setCountry(new Country("Canada"));
+                    user.setCity(new City(user.getCountry(), "Montreal"));
+                    user.setGroup(new Group(user.getCity(), "group3"));
+                    user.setPlatoon(new Platoon(user.getGroup(), "platoon3"));
+                    user.setSection(new Section(user.getPlatoon(), "section3"));
                     break;
             }
             authorityRepository.save(authority);
-            user.setAuthorities(new HashSet<Authority>(){{ add(authority); }});
+            user.setAuthorities(new HashSet<Authority>() {{
+                add(authority);
+            }});
             users.add(user);
         });
         userRepository.save(users);
     }
-
 }
