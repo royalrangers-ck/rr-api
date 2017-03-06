f8eaa76a065e603231daf0b2745075d1fba9cf26
KhomchenkoAlex <brenn@ukr.net>
2017-03-06 10:34:51 +0200

    Update UserRepository.java
    Revome commented code.

--- src/main/java/com/royalrangers/security/repository/UserRepository.java
+++ src/main/java/com/royalrangers/security/repository/UserRepository.java
@@ -4,11 +4,11 @@ import com.royalrangers.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+   
     User findByUsername(String username);
     User findByEmail(String email);
-    //List<User> findAll();
+  
 }
