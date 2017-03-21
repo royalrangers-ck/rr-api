60952a60c7240346ba93f77b2431cf188a144a07
Lexa <alex341@ukr.net>
2017-03-21 22:54:12 +0200

    move all logic for upload file to Dropbox in DropboxService

--- src/main/java/com/royalrangers/controller/DropboxController.java
+++ src/main/java/com/royalrangers/controller/DropboxController.java
@@ -1,8 +1,9 @@
 package com.royalrangers.controller;
 
 import com.dropbox.core.DbxException;
-import com.dropbox.core.v2.files.FileMetadata;
+import com.royalrangers.bean.ResponseResult;
 import com.royalrangers.service.DropboxService;
+import com.royalrangers.utils.ResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,16 +18,11 @@ public class DropboxController {
     DropboxService dropboxService;
 
     @PostMapping("/upload")
-    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException, DbxException {
-
+    public ResponseResult upload(@RequestParam("file") MultipartFile file) throws IOException, DbxException {
         if (file.isEmpty()) {
-            return "Please select a file to upload";
+            return ResponseBuilder.success("Please select a file to upload");
         }
-        byte[] bytes = file.getBytes();
-        InputStream in = new ByteArrayInputStream(bytes);
-        FileMetadata metadata = dropboxService.getClient().files().uploadBuilder("/" + file.getOriginalFilename())
-                .uploadAndFinish(in);
-
-        return "You successfully uploaded '" + file.getOriginalFilename() + "'";
+        dropboxService.fileUpload(file);
+        return ResponseBuilder.success("You successfully uploaded '" + file.getOriginalFilename() + "'");
     }
 }
--- src/main/java/com/royalrangers/service/DropboxService.java
+++ src/main/java/com/royalrangers/service/DropboxService.java
@@ -1,10 +1,16 @@
 package com.royalrangers.service;
 
+import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 
 @Service
@@ -18,5 +24,12 @@ public class DropboxService {
     DbxRequestConfig config = new DbxRequestConfig(appName, Locale.getDefault().toString());
     DbxClientV2 client = new DbxClientV2(config, accessToken);
         return client;
+    }
+
+    public void fileUpload(MultipartFile file) throws IOException, DbxException {
+        byte[] bytes = file.getBytes();
+        InputStream in = new ByteArrayInputStream(bytes);
+        FileMetadata metadata = getClient().files().uploadBuilder("/" + file.getOriginalFilename())
+                .uploadAndFinish(in);
     }
 }
