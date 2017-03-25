1258de37c4058162db65e6441f4e2c22587cc522
AndriyChernienko <chernienkoandriy@gmail.com>
2017-03-25 10:30:52 +0200

    repository for achievements moved  from package dao to repository

--- src/main/java/com/royalrangers/repository/QuarterAchievementRepository.java
+++ src/main/java/com/royalrangers/repository/QuarterAchievementRepository.java
@@ -1,4 +1,4 @@
-package com.royalrangers.dao;
+package com.royalrangers.repository;
 
 import com.royalrangers.model.achievement.QuarterAchievement;
 import org.springframework.data.jpa.repository.JpaRepository;
