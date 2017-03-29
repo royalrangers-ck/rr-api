3bf4bc898afa8e21fc45ee41462e602dfe713b28
Denis Zamosenchuk <denis.zamosenchuk@gmail.com>
2017-03-29 17:55:36 +0300

    Merge branch 'develop' into feature/user_update
    # Conflicts:
    #	src/main/java/com/royalrangers/controller/UserController.java
    #	src/main/java/com/royalrangers/service/UserService.java

--- src/main/java/com/royalrangers/controller/UserController.java
+++ src/main/java/com/royalrangers/controller/UserController.java
@@ -9,10 +9,10 @@ import com.royalrangers.service.UserService;
 import com.royalrangers.utils.ResponseBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
