ede5b7036e943d2dea1ed3b19dc8eb5509583e33
Lexa <alex341@ukr.net>
2017-04-02 13:39:03 +0300

    update AdminService

--- src/main/java/com/royalrangers/service/AdminService.java
+++ src/main/java/com/royalrangers/service/AdminService.java
@@ -3,6 +3,7 @@ package com.royalrangers.service;
 import com.royalrangers.bean.ResponseResult;
 import com.royalrangers.model.User;
 import com.royalrangers.repository.AuthorityRepository;
+import com.royalrangers.service.achievement.UserTwelveYearAchievementService;
 import com.royalrangers.utils.ResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,7 @@ public class AdminService {
 
         if (user.getAuthorities().contains(authorityRepository.findOne(2L))) {
 
-            return ResponseBuilder.success(userTwelveYearAchievementService.getAllUserWithAchievement());
+            return ResponseBuilder.success(userTwelveYearAchievementService.findAllForUser());
         }
         return ResponseBuilder.fail("This information can see only admin");
     }
