5a313b76dfb37011ef430c5dcaf9d8f8606c81e5
Khomchenko Alex <brenn@ukr.net>
2017-04-06 10:18:37 +0300

    Add @RequestParam to controller arguments.

--- src/main/java/com/royalrangers/controller/achievement/UserAchievementController.java
+++ src/main/java/com/royalrangers/controller/achievement/UserAchievementController.java
@@ -6,8 +6,10 @@ import com.royalrangers.model.achievement.*;
 import com.royalrangers.service.achievement.*;
 import com.royalrangers.utils.ResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -34,42 +36,60 @@ public class UserAchievementController {
 
 
     @GetMapping("/task")
-    public ResponseResult getUsersListForTask(Long platoonId, Long taskId, String achievementState){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseResult getUsersListForTask(@RequestParam Long platoonId,
+                                              @RequestParam Long taskId,
+                                              @RequestParam String achievementState){
         List<UserTask> userAchievementList = userTaskService.getUserTasksByTaskId(taskId);
         List<UserBean> result = getUsersFromUserAchievements(userAchievementList, platoonId,achievementState);
         return ResponseBuilder.success(result);
     }
 
     @GetMapping("/test")
-    public ResponseResult getUsersListForTest(Long platoonId, Long testId, String achievementState){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseResult getUsersListForTest(@RequestParam Long platoonId,
+                                              @RequestParam Long testId,
+                                              @RequestParam String achievementState){
         List<UserTest> userAchievementList = userTestService.getUserTestsByTestId(testId);
         List<UserBean> result = getUsersFromUserAchievements(userAchievementList, platoonId,achievementState);
         return ResponseBuilder.success(result);
     }
 
     @GetMapping("/quarterAchievement")
-    public ResponseResult getUsersListForQuarterAchievement(Long platoonId, Long achievementId, String achievementState){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseResult getUsersListForQuarterAchievement(@RequestParam Long platoonId,
+                                                            @RequestParam Long achievementId,
+                                                            @RequestParam String achievementState){
         List<UserQuarterAchievement> userAchievementList = userQuarterAchievementService.getUserQuarterAchievementByAchievementId(achievementId);
         List<UserBean> result = getUsersFromUserAchievements(userAchievementList, platoonId,achievementState);
         return ResponseBuilder.success(result);
     }
 
     @GetMapping("/yearAchievement")
-    public ResponseResult getUsersListForYearAchievement(Long platoonId, Long achievementId, String achievementState){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseResult getUsersListForYearAchievement(@RequestParam Long platoonId,
+                                                         @RequestParam Long achievementId,
+                                                         @RequestParam String achievementState){
         List<UserYearAchievement> userAchievementList = userYearAchievementService.getUserYearAchievementByAchievementId(achievementId);
         List<UserBean> result = getUsersFromUserAchievements(userAchievementList, platoonId,achievementState);
         return ResponseBuilder.success(result);
     }
 
     @GetMapping("/threeYearAchievement")
-    public ResponseResult getUsersListForThreeYearAchievement(Long platoonId, Long achievementId, String achievementState){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseResult getUsersListForThreeYearAchievement(@RequestParam Long platoonId,
+                                                              @RequestParam Long achievementId,
+                                                              @RequestParam String achievementState){
         List<UserThreeYearAchievement> userAchievementList = userThreeYearAchievementService.getUserThreeYearAchievementByAchievementId(achievementId);
         List<UserBean> result = getUsersFromUserAchievements(userAchievementList, platoonId,achievementState);
         return ResponseBuilder.success(result);
     }
 
     @GetMapping("/twelveYearAchievement")
-    public ResponseResult getUsersListForTwelveYearAchievement(Long platoonId, Long achievementId, String achievementState){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseResult getUsersListForTwelveYearAchievement(@RequestParam Long platoonId,
+                                                               @RequestParam Long achievementId,
+                                                               @RequestParam String achievementState){
         List<UserTwelveYearAchievement> userAchievementList = userTwelveYearAchievementService.getUserTwelveYearAchievementByAchievementId(achievementId);
         List<UserBean> result = getUsersFromUserAchievements(userAchievementList, platoonId,achievementState);
         return ResponseBuilder.success(result);
