1ad3297dce0291231c1dab03a83420a81c6dd198
Khomchenko Alex <brenn@ukr.net>
2017-04-26 23:30:57 +0300

    Fix import.

--- src/main/java/com/royalrangers/controller/UserController.java
+++ src/main/java/com/royalrangers/controller/UserController.java
@@ -13,7 +13,6 @@ import com.royalrangers.utils.ResponseBuilder;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
